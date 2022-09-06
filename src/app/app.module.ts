7997a36b503b207052e3c06dcd8b4d3ada37811a
nikhiljanshali <nikhil.janshali@gmail.com>
2022-09-06 14:05:17 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CurrencytowordService } from './service/currencytoword.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { VoterComponent } from './ui/voter/voter.component';
-import { AmountWordComponent } from './ui/amount-word/amount-word.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './shared/component/header/header.component';
 import { NavTabComponent } from './shared/component/nav-tab/nav-tab.component';
@@ -18,8 +16,6 @@ import { NavTabComponent } from './shared/component/nav-tab/nav-tab.component';
     AppComponent,
     HeaderComponent,
     NavTabComponent,
-    VoterComponent,
-    AmountWordComponent,
   ],
   bootstrap: [AppComponent],
 })
