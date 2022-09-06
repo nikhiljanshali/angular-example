85ee1c89d1ffea1b9d6508a7c9fe190251ab0959
nikhiljanshali <nikhil.janshali@gmail.com>
2022-09-06 14:08:08 +0530

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageNotFoundComponent } from './ui/page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/superheroes', pathMatch: 'full' },
