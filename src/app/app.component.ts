70ea6fada424c7413265d863c44f5cbb90eb0208
nikhiljanshali <nikhil.janshali@gmail.com>
2022-09-07 09:36:28 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, VERSION } from '@angular/core';
-import { listCurrencyType } from './constant/static-const';
-import { CurrencytowordService } from './service/currencytoword.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -8,8 +6,6 @@ import { CurrencytowordService } from './service/currencytoword.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  
-
   constructor() {}
 
   ngOnInit(): void {
