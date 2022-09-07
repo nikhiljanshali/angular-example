ad0a52b069e86c3b1a64fdf95194eaf230bd3701
nikhiljanshali <nikhil.janshali@gmail.com>
2022-09-07 10:31:37 +0530

    1 file modified

--- src/app/service/date-compare.service.ts
+++ src/app/service/date-compare.service.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Injectable } from '@angular/core';
 import {
   CompareValidator,
@@ -8,14 +9,68 @@ import {
 export class DateCompareService {
   private compareValidator = CompareValidator;
   private compareValueValidator = CompareValueValidator;
-  constructor() {}
+
+  constructor(private datePipe: DatePipe) {}
 
   public compareInputDateWithTodayDate(
     inputDate: Date,
     compareType: string
   ): string {
-    const dateOne = new Date(inputDate);
-    const dateTwo = new Date();
+    const dateOne = this.datePipe.transform(new Date(inputDate), 'dd/MM/yyyy');
+    const dateTwo = this.datePipe.transform(new Date(), 'dd/MM/yyyy');
+
+    console.info('Input Date => ', dateOne);
+    console.info('Today Date => ', dateTwo);
+
+    let flag: string = '';
+    // 1. Abstract equality check
+    if (this.compareValidator.EqualTo == compareType) {
+      if (dateOne == dateTwo) {
+        flag = this.compareValueValidator.$Equal;
+      } else {
+        flag = this.compareValueValidator.$NotEqual;
+      }
+    }
+
+    // 2. Strict equality check
+    if (this.compareValidator.StrictEqualTo == compareType) {
+      if (dateOne === dateTwo) {
+        flag = this.compareValueValidator.$Equal;
+      } else {
+        flag = this.compareValueValidator.$NotEqual;
+      }
+    }
+
+    // 3. Greater than check
+    if (this.compareValidator.GreaterThan == compareType) {
+      if (dateOne > dateTwo) {
+        flag = this.compareValueValidator.$GreaterThan;
+      } else {
+        flag = this.compareValueValidator.$NotGreaterThan;
+      }
+    }
+
+    // 4. Less than check
+    if (this.compareValidator.LessThan == compareType) {
+      if (dateOne < dateTwo) {
+        flag = this.compareValueValidator.$LessThan;
+      } else {
+        flag = this.compareValueValidator.$NotLessThan;
+      }
+    }
+    return flag;
+  }
+
+  public compareInputTimeWithCurrentTime(
+    inputDate: Date,
+    compareType: string
+  ): string {
+    const dateOne = this.datePipe.transform(new Date(inputDate), 'hh:mm:ss');
+    const dateTwo = this.datePipe.transform(new Date(), 'hh:mm:ss');
+
+    console.info('Input Time => ', dateOne);
+    console.info('Current Time => ', dateTwo);
+
     let flag: string = '';
     // 1. Abstract equality check
     if (this.compareValidator.EqualTo == compareType) {
