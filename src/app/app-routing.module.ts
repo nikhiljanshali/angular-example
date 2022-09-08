9b75f9f7eb29bdb9be849c38fc4be3a8871fe0cf
nikhiljanshali <nikhil.janshali@gmail.com>
2022-09-08 17:06:25 +0530

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { DateCompareComponent } from './pages/date-compare/date-compare.componen
 import { PageNotFoundComponent } from './shared/component/page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: AmountWordComponent },
   { path: 'amountword', component: AmountWordComponent },
   { path: 'datetimecompare', component: DateCompareComponent },
   { path: '**', component: PageNotFoundComponent },
