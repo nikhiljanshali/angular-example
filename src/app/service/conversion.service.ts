2c0e4a9dbb274590290ef12f1d2d99abcce2f4d5
nikhiljanshali <nikhil.janshali@gmail.com>
2022-09-10 11:50:12 +0530

    1 file modified

--- src/app/service/conversion.service.ts
+++ src/app/service/conversion.service.ts
@@ -2,10 +2,16 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ConversionService {
+  /**
+   * variable declaration
+   */
   public _compareValidator: Array<{ id: string; name: string }> = [];
 
   constructor() {}
 
+  /**
+   * convert Enum to KeyValuePair Object. (loosely coupled e.g, any keyword)
+   */
   public convertEnumToKeyValuePairObject(inputEnum: any): any {
     for (var n in inputEnum) {
       this._compareValidator.push({ id: <string>inputEnum[n], name: n });
